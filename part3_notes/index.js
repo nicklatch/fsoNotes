af55cff21e4da5a4e001b1c227036c57a006a2f3
Nick Latcham <nick.latcham@proton.me>
2023-04-30 10:24:37 -0500

    .

--- part3_notes/index.js
+++ part3_notes/index.js
@@ -2,7 +2,6 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const process = require("process");
-//const mongoose = require("mongoose");
 const Note = require("./models/note");
 const app = express();
 
