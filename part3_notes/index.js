11f84e076472ae5b41f2b0e50a8f9d65361346e8
Nick Latcham <nick.latcham@proton.me>
2023-04-30 10:05:16 -0500

    uibuild

--- part3_notes/index.js
+++ part3_notes/index.js
@@ -1,13 +1,16 @@
+require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const process = require("process");
+//const mongoose = require("mongoose");
+const Note = require("./models/note");
 const app = express();
 
 const requestLogger = (request, response, next) => {
   console.log("Method:", request.method);
   console.log("Path:  ", request.path);
   console.log("Body:  ", request.body);
-  console.log("---");
+  console.log("---------------------");
   next();
 };
 
@@ -20,69 +23,39 @@ app.use(cors());
 app.use(express.json());
 app.use(requestLogger);
 
-let notes = [
-  {
-    id: 1,
-    content: "HTML is easy",
-    important: true,
-  },
-  {
-    id: 2,
-    content: "Browser can execute only JavaScript",
-    important: false,
-  },
-  {
-    id: 3,
-    content: "GET and POST are the most important methods of HTTP protocol",
-    important: true,
-  },
-];
-
-app.get("/", (req, res) => {
-  res.send("<h1>Hello World!</h1>");
+app.get("/", (request, response) => {
+  response.send("<h1>Hello World!</h1>");
 });
 
-app.get("/api/notes", (req, res) => {
-  res.json(notes);
+app.get("/api/notes", (request, response) => {
+  Note.find({}).then((notes) => {
+    response.json(notes);
+  });
 });
-
-const generateId = () => {
-  const maxId = notes.length > 0 ? Math.max(...notes.map((n) => n.id)) : 0;
-  return maxId + 1;
-};
 
 app.post("/api/notes", (request, response) => {
   const body = request.body;
 
-  if (!body.content) {
+  if (body.content === undefined) {
     return response.status(400).json({
       error: "content missing",
     });
   }
 
-  const note = {
+  const note = new Note({
     content: body.content,
     important: body.important || false,
-    date: new Date(),
-    id: generateId(),
-  };
+  });
 
-  notes = notes.concat(note);
-
-  response.json(note);
+  note.save().then((savedNote) => {
+    response.json(savedNote);
+  });
 });
 
 app.get("/api/notes/:id", (request, response) => {
-  const id = Number(request.params.id);
-  const note = notes.find((note) => note.id === id);
-
-  if (note) {
+  Note.findById(request.params.id).then((note) => {
     response.json(note);
-  } else {
-    response.status(404).end();
-  }
-
-  response.json(note);
+  });
 });
 
 app.delete("/api/notes/:id", (request, response) => {
@@ -94,7 +67,7 @@ app.delete("/api/notes/:id", (request, response) => {
 
 app.use(unknownEndpoint);
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
-  console.log(`${process.cwd()} | Server running on port ${PORT}`);
+  console.log(`${process.cwd()} | Listening on port ${PORT}`);
 });
--- part3_notes/models/note.js
+++ part3_notes/models/note.js
@@ -3,13 +3,16 @@ const mongoose = require("mongoose");
 mongoose.set("strictQuery", false);
 
 const url = process.env.MONGODB_URI;
-
+console.log("--------------------");
 console.log(`connecting to ${url}`);
+console.log("--------------------");
 
 mongoose
   .connect(url)
   .then((result) => {
+    console.log("--------------------");
     console.log("connected to MongoDB");
+    console.log("--------------------");
   })
   .catch((error) => {
     console.log(`error conneting to MongoDB: ${error.message}`);
