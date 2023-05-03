09f67e53d358fbe07d4e5727f15fba1c622f0b80
Nick Latcham <nick.latcham@proton.me>
2023-05-03 14:59:05 -0500

    eslint config

--- part3_notes/index.js
+++ part3_notes/index.js
@@ -1,50 +1,50 @@
-require("dotenv").config();
-const express = require("express");
-const cors = require("cors");
-const process = require("process");
-const Note = require("./models/note");
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const process = require('process');
+const Note = require('./models/note');
 const app = express();
 
 const requestLogger = (request, response, next) => {
-  console.log("Method:", request.method);
-  console.log("Path:  ", request.path);
-  console.log("Body:  ", request.body);
-  console.log("***");
+  console.log('Method:', request.method);
+  console.log('Path:  ', request.path);
+  console.log('Body:  ', request.body);
+  console.log('***');
   next();
 };
 
 const errorHandler = (error, request, response, next) => {
   console.log(error.message);
 
-  if (error.name === "CastError") {
-    return response.status(400).send({ error: "malformatted id" });
-  } else if (error.name === "ValidationError") {
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' });
+  } else if (error.name === 'ValidationError') {
     return response.status(400).json({ error: error.message });
   }
   next(error);
 };
 
 const unknownEndpoint = (request, response) => {
-  response.status(404).send({ error: "unknown endpoint" });
+  response.status(404).send({ error: 'unknown endpoint' });
 };
 
 app.use(cors());
 app.use(express.json());
 app.use(requestLogger);
-app.use(express.static("dist"));
+app.use(express.static('dist'));
 
-app.get("/api/notes", (request, response) => {
+app.get('/api/notes', (request, response) => {
   Note.find({}).then((notes) => {
     response.json(notes);
   });
 });
 
-app.post("/api/notes", (request, response, next) => {
+app.post('/api/notes', (request, response, next) => {
   const body = request.body;
 
   if (body.content === undefined) {
     return response.status(400).json({
-      error: "content missing",
+      error: 'content missing',
     });
   }
 
@@ -61,7 +61,7 @@ app.post("/api/notes", (request, response, next) => {
     .catch((error) => next(error));
 });
 
-app.get("/api/notes/:id", (request, response, next) => {
+app.get('/api/notes/:id', (request, response, next) => {
   Note.findById(request.params.id)
     .then((note) => {
       note ? response.json(note) : response.status(404).end();
@@ -69,21 +69,21 @@ app.get("/api/notes/:id", (request, response, next) => {
     .catch((error) => next(error));
 });
 
-app.delete("/api/notes/:id", (request, response, next) => {
+app.delete('/api/notes/:id', (request, response, next) => {
   Note.findByIdAndRemove(request.params.id)
-    .then((result) => {
+    .then((response) => {
       response.status(204).end();
     })
     .catch((error) => next(error));
 });
 
-app.put("/api/notes/:id", (request, response, next) => {
+app.put('/api/notes/:id', (request, response, next) => {
   const { content, important } = request.body;
 
   Note.findByIdAndUpdate(
     request.params.id,
     { content, important },
-    { new: true, runValidators: true, context: "query" }
+    { new: true, runValidators: true, context: 'query' }
   )
     .then((updatedNote) => {
       response.json(updatedNote);
