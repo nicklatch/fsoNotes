a39cf841f72c178852bb1657e6059d6afb9e297a
Nick Latcham <nick.latcham@proton.me>
2023-05-06 09:23:58 -0500

    .

--- part4_notes/mongo.js
+++ part4_notes/mongo.js
@@ -1,15 +1,11 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
+require('dotenv').config();
 
-if (process.argv.length < 3) {
-  console.log("give password as argument");
-  process.exit(1);
-}
+const password = process.env.MONGODB_PW;
 
-const password = process.argv[2];
+const url = `mongodb+srv://nicklatcham:${password}@fullstackone.lxnhe7w.mongodb.net/testNoteApp?retryWrites=true&w=majority`;
 
-const url = `mongodb+srv://nicklatcham:${password}@fullstackone.lxnhe7w.mongodb.net/noteApp?retryWrites=true&w=majority`;
-
-mongoose.set("strictQuery", false);
+mongoose.set('strictQuery', false);
 mongoose.connect(url);
 
 const noteSchema = new mongoose.Schema({
@@ -17,21 +13,21 @@ const noteSchema = new mongoose.Schema({
   important: Boolean,
 });
 
-const Note = mongoose.model("Note", noteSchema);
+const Note = mongoose.model('Note', noteSchema);
 
-// const note = new Note({
-//   content: "GET and POST are the most important methods of HTTP protocol",
-//   important: true,
-// });
+const note = new Note({
+  content: 'supertest(api) is called a superagent',
+  important: false,
+});
 
-// note.save().then((result) => {
-//   console.log("note saved!");
-//   mongoose.connection.close();
-// });
-
-Note.find({}).then((result) => {
-  result.forEach((note) => {
-    console.log(note);
-  });
+note.save().then((result) => {
+  console.log('note saved!');
   mongoose.connection.close();
 });
+
+// Note.find({}).then((result) => {
+//   result.forEach((note) => {
+//     console.log(note);
+//   });
+//   mongoose.connection.close();
+// });
