1b4801601a5d0e9299473f167d84e9635f557600
Nick Latcham <nick.latcham@proton.me>
2023-05-07 21:30:47 -0500

    formatting

--- part4_notes/controllers/notes.js
+++ part4_notes/controllers/notes.js
@@ -6,28 +6,27 @@ notesRouter.get('/', async (request, response) => {
   response.json(notes);
 });
 
-notesRouter.get('/:id', (request, response, next) => {
-  Note.findById(request.params.id)
-    .then((note) => {
-      note ? response.json(note) : response.status(404).end();
-    })
-    .catch((error) => next(error));
+notesRouter.get('/:id', async (request, response, next) => {
+  try {
+    const note = await Note.findById(request.params.id);
+    note ? response.json(note) : response.status(404).end();
+  } catch (exception) {
+    next(exception);
+  }
 });
 
-notesRouter.post('/', (request, response, next) => {
+notesRouter.post('/', async (request, response, next) => {
   const body = request.body;
-
   const note = new Note({
     content: body.content,
     important: body.important || false,
   });
-
-  note
-    .save()
-    .then((savedNote) => {
-      response.status(201).json(savedNote);
-    })
-    .catch((error) => next(error));
+  try {
+    const savedNote = await note.save();
+    response.status(201).json(savedNote);
+  } catch (exception) {
+    next(exception);
+  }
 });
 
 notesRouter.delete('/:id', (request, response, next) => {
