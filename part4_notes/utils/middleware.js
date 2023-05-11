e2ab2617bcd1fa1b4c04fbc1cfa72f82385137d6
Nick <nick.latcham@proton.me>
2023-05-11 16:15:21 -0500

    .

--- part4_notes/utils/middleware.js
+++ part4_notes/utils/middleware.js
@@ -12,21 +12,17 @@ const unknownEndpoint = (request, response) => {
   response.status(400).send({ error: 'Unknown Endpoint' });
 };
 
-const errorHandler = (error, requst, response, next) => {
+const errorHandler = (error, request, response, next) => {
   logger.error(error.message);
 
-  switch (error.name) {
-    case 'CastError':
-      return response.status(400).send({ error: 'Malformated ID' });
-    case 'ValidationError':
-      return response.status(400).json({ error: error.message });
-    case 'JsonWebTokenError':
-      return response.status(401).json({ error: error.message });
-    case 'TokenExpiredError':
-      return response.status(401).json({ error: 'token expired' });
-    default:
-      logger.error(error.message);
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' });
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message });
+  } else if (error.name === 'JsonWebTokenError') {
+    return response.status(400).json({ error: error.message });
   }
+
   next(error);
 };
 
