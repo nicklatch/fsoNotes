5d32642c3c647a9f05230062416f6d827b4ad175
Nick Latcham <nick.latcham@proton.me>
2023-06-04 21:23:36 -0500

    .

--- part6_notes/src/App.jsx
+++ part6_notes/src/App.jsx
@@ -1,11 +1,5 @@
 import { createStore } from 'redux';
-
-const noteReducer = (state = [], action) => {
-  if (action.type === 'NEW_NOTE') {
-    return state.concat(action.payload);
-  }
-  return state;
-};
+import noteReducer from './reducers/noteReducer';
 
 const store = createStore(noteReducer);
 
---
+++ part6_notes/src/reducers/noteReducer.test.js
@@ -0,0 +1,4 @@
+import noteReducer from './noteReducer';
+import deepFreeze from 'deep-freeze';
+
+//TODO: pick back up at "Pure functions, immutable" after the npm install
