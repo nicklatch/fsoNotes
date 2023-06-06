1ccadb2e704f5b968b10eac0c848030ed1bf6d2a
Nick <nick.latcham@proton.me>
2023-06-06 13:08:14 -0500

    .

--- part6_notes/src/reducers/noteReducer.js
+++ part6_notes/src/reducers/noteReducer.js
@@ -1,8 +1,19 @@
 const noteReducer = (state = [], action) => {
-  if (action.type === 'NEW_NOTE') {
-    return state.concat(action.payload);
+  switch (action.type) {
+    case 'NEW_NOTE':
+      return [...state, action.payload];
+    case 'TOGGLE_IMPORTANCE': {
+      const id = action.payload.id;
+      const noteToChange = state.find((n) => n.id === id);
+      const changedNote = {
+        ...noteToChange,
+        important: !noteToChange.important,
+      };
+      return state.map((note) => (note.id !== id ? note : changedNote));
+    }
+    default:
+      return state;
   }
-  return state;
 };
 
 export default noteReducer;
